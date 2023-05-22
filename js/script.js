7b88b49285384bb28b28939d66b2c799224bfdde
infodevestudent <infodev.infodev@gmail.com>
2023-05-22 18:16:25 -0300

    primeiro ponto

---
+++ js/script.js
@@ -0,0 +1,9 @@
+let elementosLi = document.querySelectorAll(".container-img li");
+let modelEl = document.querySelector("#modal");
+let index = 0;
+
+function show(increase){
+    index = index + increase;;
+    index = Math.min(Math.max(index, 0), elementosLi.length - 1);
+    elementosLi[index].scrollIntoView({ behavior: 'smooth' });
+}
