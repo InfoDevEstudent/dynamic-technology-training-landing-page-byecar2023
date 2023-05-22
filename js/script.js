e2f46e3a515feeceda1fc42be77d7d1e5b251dbb
infodevestudent <infodev.infodev@gmail.com>
2023-05-22 18:23:01 -0300

    up1

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ let modelEl = document.querySelector("#modal");
 let index = 0;
 
 function show(increase){
-    index = index + increase;;
+    index = index + increase;
     index = Math.min(Math.max(index, 0), elementosLi.length - 1);
     elementosLi[index].scrollIntoView({ behavior: 'smooth' });
 }
